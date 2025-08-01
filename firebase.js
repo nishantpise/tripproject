ac6ed1b32233f7441315331690f1ab1c7590c811
nishantpise <nishantpise1718@gmail.com>
2025-08-02 00:02:15 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// firebase.js
+
+// ✅ Only works with Firebase v8 style
+var firebaseConfig = {
+  apiKey: "AIzaSyCyIk56mSd_74ywFqKSVMwI9fma-zCGnbI",
+  authDomain: "trip-budget-tracker-e36ad.firebaseapp.com",
+  databaseURL: "https://trip-budget-tracker-e36ad-default-rtdb.firebaseio.com",
+  projectId: "trip-budget-tracker-e36ad",
+  storageBucket: "trip-budget-tracker-e36ad.appspot.com",
+  messagingSenderId: "312674525838",
+  appId: "1:312674525838:web:a9c23787900a9a88451fdf",
+  measurementId: "G-5VB2GHHR3R"
+};
+
+// ✅ Initialize Firebase
+firebase.initializeApp(firebaseConfig);
